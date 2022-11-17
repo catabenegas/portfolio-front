55015e18409155b1e7e3814b3ab4e24d07b4867c
Cathutu <catalinabenegas@gmail.com>
2022-11-17 13:07:52 -0300

    Update edu.service.ts

--- src/app/services/edu.service.ts
+++ src/app/services/edu.service.ts
@@ -9,7 +9,7 @@ const cabecera = {headers: new HttpHeaders({'Content-Type': 'application/json'})
   providedIn: 'root'
 })
 export class EduService {
-  eduURL = 'http://portfolio-back3233.herokuapp.comapi/edu/'
+  eduURL = 'https://portfolio-back3233.herokuapp.comapi/edu/'
 
   constructor(private httpClient: HttpClient) { }
 
